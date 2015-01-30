d060b4b96e3d56edee942404a7ccfba39990a1e8
sirolf2009 <masterflappie@gmail.com>
2015-01-30 16:47:14 +0100

    Updated to libGDX 1.5.3

---
+++ war-triumph-html/src/com/sirolf2009/wartriumph/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.sirolf2009.wartriumph.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sirolf2009.wartriumph.WarTriumph;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new WarTriumph();
+        }
+}
